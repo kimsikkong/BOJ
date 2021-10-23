90687af15d4eea2bfd53add60b6c04106e223570
kimsikkong <kimsikkong@gmail.com>
2021-10-23 18:50:29 +0900

    11053 - 가장 긴 증가하는 부분 수열 문제 풀이 추가

---
+++ src/q_11053/Main.java
@@ -0,0 +1,41 @@
+package q_11053;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        dp = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i - 1; j >= 0 ; j--) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(dp[i], max);
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(max + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
