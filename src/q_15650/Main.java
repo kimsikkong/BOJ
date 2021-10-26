dfa10e63e26f3a748c58de18f20ec0a2320b69ee
kimsikkong <kimsikkong@gmail.com>
2021-10-26 17:46:16 +0900

    15650 - N과 M(2) 문제 풀이 추가

---
+++ src/q_15650/Main.java
@@ -0,0 +1,44 @@
+package q_15650;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr;
+    static int[] answer;
+    static int N;
+    static int M;
+    static StringBuilder builder = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        answer = new int[M];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+
+        Main T = new Main();
+        T.backtracking(0, 0);
+        System.out.println(builder.toString());
+    }
+
+    private void backtracking(int idx, int l) {
+        if (l == M) {
+            for (int i = 0; i < M; i++) {
+                builder.append(answer[i]).append(" ");
+            }
+            builder.append("\n");
+        } else {
+            for (int i = idx; i < N; i++) {
+                answer[l] = arr[i];
+                backtracking(i + 1, l + 1);
+            }
+        }
+    }
+}
